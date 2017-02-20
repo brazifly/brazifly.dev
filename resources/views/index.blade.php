607f6c05096f7f8c819487360fb0c36172806c95
brazifly <brazifly@nvsdi.com>
2017-02-20 13:17:46 -0300

    seed

---
+++ resources/views/index.blade.php
@@ -0,0 +1,314 @@
+@extends('layouts/default')
+
+{{-- Page title --}}
+@section('title')
+Home
+@parent
+@stop
+
+{{-- page level styles --}}
+@section('header_styles')
+    <!--page level css starts-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/frontend/tabbular.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/animate/animate.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/frontend/jquery.circliful.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/owl_carousel/css/owl.carousel.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendors/owl_carousel/css/owl.theme.css') }}">
+    <!--end of page level css-->
+@stop
+
+{{-- slider --}}
+@section('top')
+    <!--Carousel Start -->
+    <div id="owl-demo" class="owl-carousel owl-theme">
+        <div class="item"><img src="{{ asset('assets/images/slide_1.jpg') }}" alt="slider-image">
+        </div>
+        <div class="item"><img src="{{ asset('assets/images/slide_2.jpg') }}" alt="slider-image">
+        </div>
+        <div class="item"><img src="{{ asset('assets/images/slide_3.jpg') }}" alt="slider-image">
+        </div>
+    </div>
+    <!-- //Carousel End -->
+@stop
+
+{{-- content --}}
+@section('content')
+    <div class="container">
+        <section class="purchas-main">
+            <div class="container bg-border wow pulse" data-wow-duration="2.5s">
+                <div class="row">
+                    <div class="col-md-7 col-sm-7 col-xs-12">
+                        <h1 class="purchae-hed">BraziFly Inventory</h1></div>
+                    <div class="col-md-5 col-sm-5 col-xs-12"><a href="#" class="btn btn-primary purchase-styl pull-right">Purchase now</a></div>
+                </div>
+            </div>
+        </section>
+        <!-- Service Section Start-->
+        <div class="row">
+            <!-- Inventory Section Start -->
+            <div class="text-center">
+                <h3 class="border-primary"><span class="heading_border bg-primary">BraziFly Features</span></h3>
+            </div>
+            <div class="col-sm-6 col-md-3 wow bounceInLeft" data-wow-duration="3.5s">
+                <div class="box">
+                    <div class="box-icon">
+                        <i class="livicon icon" data-name="desktop" data-size="55" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>
+                    </div>
+                    <div class="info">
+                        <h3 class="success text-center">Inventory</h3>
+                        <p>Keep inventory of flyies, materials and equipment , elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                        <div class="text-right primary"><a href="#">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- //Inventory Section End -->
+            <!-- Easy to Use Section Start -->
+            <div class="col-sm-6 col-md-3 wow bounceInDown" data-wow-duration="3s" data-wow-delay="0.4s">
+                <!-- Box Start -->
+                <div class="box">
+                    <div class="box-icon box-icon1">
+                        <i class="livicon icon1" data-name="gears" data-size="55" data-loop="true" data-c="#418bca" data-hc="#418bca"></i>
+                    </div>
+                    <div class="info">
+                        <h3 class="primary text-center">Easy to Use</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!.</p>
+                        <div class="text-right primary"><a href="#">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- //Easy to Use Section End -->
+            <!-- Clean Design Section Start -->
+            <div class="col-sm-6 col-md-3 wow bounceInUp" data-wow-duration="3s" data-wow-delay="0.8s">
+                <div class="box">
+                    <div class="box-icon box-icon2">
+                        <i class="livicon icon1" data-name="doc-portrait" data-size="55" data-loop="true" data-c="#f89a14" data-hc="#f89a14"></i>
+                    </div>
+                    <div class="info">
+                        <h3 class="warning text-center">Clean Design</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                        <div class="text-right primary"><a href="#">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- //Clean Design Section End -->
+            <!-- 20+ Page Section Start -->
+            <div class="col-sm-6 col-md-3 wow bounceInRight" data-wow-duration="5s" data-wow-delay="1.2s">
+                <div class="box">
+                    <div class="box-icon box-icon3">
+                        <i class="livicon icon1" data-name="folder-open" data-size="55" data-loop="true" data-c="#FFD43C" data-hc="#FFD43C"></i>
+                    </div>
+                    <div class="info">
+                        <h3 class="yellow text-center">20+ Pages</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti atque, tenetur quam aspernatur corporis at explicabo nulla dolore necessitatibus doloremque exercitationem sequi dolorem architecto perferendis quas aperiam debitis dolor soluta!</p>
+                        <div class="text-right primary"><a href="#">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- //20+ Page Section End -->
+        </div>
+        <!-- //Services Section End -->
+        <!-- Our Team Start -->
+        <div class="row text-center">
+            <h3 class=" border-danger"><span class="heading_border bg-danger">Our Team</span></h3>
+            <div class="col-md-3 col-sm-5 profile wow fadeInLeft" data-wow-duration="3s">
+                <div class="thumbnail bg-white">
+                    <img src="{{ asset('assets/images/img_3.jpg') }}" alt="team-image" class="img-responsive">
+                    <div class="caption">
+                        <b>John Doe</b>
+                        <br /> Founder & Partner
+                        <br />
+                        <div class="divide">
+                            <a href="#" class="divider"> <i class="livicon" data-name="facebook" data-size="22" data-loop="true" data-c="#3a5795" data-hc="#3a5795"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="twitter" data-size="22" data-loop="true" data-c="#55acee" data-hc="#55acee"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="google-plus" data-size="22" data-loop="true" data-c="#d73d32" data-hc="#d73d32"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="linkedin" data-size="22" data-loop="true" data-c="#1b86bd" data-hc="#1b86bd"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-5 profile wow fadeInUp" data-wow-duration="3s" data-wow-delay="0.5s">
+                <div class="thumbnail bg-white">
+                    <img src="{{ asset('assets/images/img_5.jpg') }}" alt="team-image">
+                    <div class="caption">
+                        <b>Francina Steinberg</b>
+                        <br /> CEO
+                        <br />
+                        <div class="divide">
+                            <a href="#" class="divider"> <i class="livicon" data-name="facebook" data-size="22" data-loop="true" data-c="#3a5795" data-hc="#3a5795"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="twitter" data-size="22" data-loop="true" data-c="#55acee" data-hc="#55acee"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="google-plus" data-size="22" data-loop="true" data-c="#d73d32" data-hc="#d73d32"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="linkedin" data-size="22" data-loop="true" data-c="#1b86bd" data-hc="#1b86bd"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-5 profile wow fadeInDown" data-wow-duration="3s" data-wow-delay="1s">
+                <div class="thumbnail bg-white">
+                    <img src="{{ asset('assets/images/img_4.jpg') }}" alt="team-image" class="img-responsive">
+                    <div class="caption">
+                        <b>Audrey Sheldon</b>
+                        <br /> Executive Manager
+                        <br />
+
+                        <div class="divide">
+                            <a href="#" class="divider"> <i class="livicon" data-name="facebook" data-size="22" data-loop="true" data-c="#3a5795" data-hc="#3a5795"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="twitter" data-size="22" data-loop="true" data-c="#55acee" data-hc="#55acee"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="google-plus" data-size="22" data-loop="true" data-c="#d73d32" data-hc="#d73d32"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="linkedin" data-size="22" data-loop="true" data-c="#1b86bd" data-hc="#1b86bd"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-5 profile wow fadeInRight" data-wow-duration="3s" data-wow-delay="1.5s">
+                <div class="thumbnail bg-white">
+                    <img src="{{ asset('assets/images/img_6.jpg') }}" alt="team-image">
+                    <div class="caption">
+                        <b>Sam Bellows</b>
+                        <br /> Manager
+                        <br />
+                        <div class="divide">
+                            <a href="#" class="divider"> <i class="livicon" data-name="facebook" data-size="22" data-loop="true" data-c="#3a5795" data-hc="#3a5795"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="twitter" data-size="22" data-loop="true" data-c="#55acee" data-hc="#55acee"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="google-plus" data-size="22" data-loop="true" data-c="#d73d32" data-hc="#d73d32"></i>
+                            </a>
+                            <a href="#"> <i class="livicon" data-name="linkedin" data-size="22" data-loop="true" data-c="#1b86bd" data-hc="#1b86bd"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- //Our Team End -->
+        <!-- What we are section Start -->
+        <div class="row">
+            <!-- What we are Start -->
+            <div class="col-md-6 col-sm-6 wow zoomInLeft" data-wow-duration="3s">
+                <div class="text-left">
+                    <div>
+                        <h4 class="border-warning"><span class="heading_border bg-warning">What We Are</span></h4>
+                    </div>
+                </div>
+                <img src="{{ asset('assets/images/image_12.jpg') }}" alt="image_12" class="img-responsive">
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+                <p>
+                    <div class="text-right primary"><a href="#">Read more</a></div>
+                </p>
+            </div>
+            <!-- //What we are End -->
+            <!-- About Us Start -->
+            <div class="col-md-6 col-sm-6 wow zoomInRight" data-wow-duration="3s">
+                <div class="text-left">
+                    <div>
+                        <h4 class="border-success"><span class="heading_border bg-success">About Us</span></h4>
+                    </div>
+                </div>
+                <img src="{{ asset('assets/images/image_11.jpg') }}" alt="image_11" class="img-responsive">
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+                <p>
+                    <div class="text-right primary"><a href="#">Read more</a>
+                    </div>
+                </p>
+            </div>
+            <!-- //About Us End -->
+        </div>
+        <!-- //What we are section End -->
+        <!-- Features Start -->
+        <div class="row features">
+            <div class="text-center">
+                <div class="text-center">
+                    <h3 class=" border-warning"><span class="heading_border bg-warning">Features</span></h3>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 wow fadeInDown" data-wow-duration="3s">
+                <div>
+                    <a href="#"><i class="livicon" data-name="checked-on" data-size="22" data-loop="true" data-c="#25a3d8" data-hc="#25a3d8"></i></a>
+                    <h4><bold>Responsive Design</bold></h4>
+                </div>
+                <div>
+                    <p>
+                        <label>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."</label>
+                    </p>
+                </div>
+                <div>
+                    <a href="#"> <i class="livicon" data-name="checked-on" data-size="22" data-loop="true" data-c="#ef8424 " data-hc="#ef8424 "></i>
+                    </a>
+                    <h4><bold>Html 5</bold></h4>
+                    <p>
+                        <label>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."</label>
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 wow fadeInDown" data-wow-duration="3s" data-wow-delay="0.8s">
+                <div>
+                    <a href="#"> <i class="livicon" data-name="checked-on" data-size="22" data-loop="true" data-c="#01bc8c" data-hc="#01bc8c"></i>
+                    </a>
+                    <h4><bold>Unique Design</bold></h4>
+                    <p>
+                        <label>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."</label>
+                    </p>
+                </div>
+                <div>
+                    <a href="#"> <i class="livicon" data-name="checked-on" data-size="22" data-loop="true" data-c="#1360b3 " data-hc="#1360b3 "></i>
+                    </a>
+                    <h4><bold>Css</bold></h4>
+                    <p>
+                        <label>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."</label>
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-4 wow fadeInDown" data-wow-duration="3s" data-wow-delay="1.2s">
+                <div>
+                    <a href="#"> <i class="livicon" data-name="checked-on" data-size="22" data-loop="true" data-c="#FFD43C" data-hc="#FFD43C"></i>
+                    </a>
+                    <h4>Clean Design</h4>
+                    <p>
+                        <label>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."</label>
+                    </p>
+                </div>
+                <div>
+                    <a href="#"> <i class="livicon" data-name="checked-on" data-size="22" data-loop="true" data-c="#91d659 " data-hc="#91d659 "></i>
+                    </a>
+                    <h4>Bootstrap</h4>
+                    <p>
+                        <label>"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."</label>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- //Features End -->
+    </div>
+    <!-- //Container End -->
+@stop
+
+{{-- footer scripts --}}
+@section('footer_scripts')
+    <!-- page level js starts-->
+    <script type="text/javascript" src="{{ asset('assets/js/frontend/jquery.circliful.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/vendors/wow/js/wow.min.js') }}" ></script>
+    <script type="text/javascript" src="{{ asset('assets/vendors/owl_carousel/js/owl.carousel.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/frontend/carousel.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('assets/js/frontend/index.js') }}"></script>
+    <!--page level js ends-->
+@stop
